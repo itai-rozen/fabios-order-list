6d2808ea74fd6cae753a0d264d7d852224c0660b
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-27 01:19:29 +0200

    when checked, cpt updates state of parent cpt

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import { JSX } from 'react'
+import { ReactNode, useState } from 'react'
+import './order.css'
 export interface OrderType {
-  id: number,
-  date: string,
-  time: string,
-  notes: string,
-  prediction: boolean,
-  branch: string,
-  branch_id: number,
-  order_type: string|null,
-  event_id: number|null,
-  recurrence: number,
-  customer: string,
-  customer_id: number,
-  num_of_guests: number|null,
-  source: string,
-  created_at: string,
-  updated_at: string,
-  status: string
+  orderDetails: {
+    id: number,
+    date: string,
+    time: string,
+    notes: string,
+    prediction: boolean,
+    branch: string,
+    branch_id: number,
+    order_type: string|null,
+    event_id: number|null,
+    recurrence: number,
+    customer: string,
+    customer_id: number,
+    num_of_guests: number|null,
+    source: string,
+    created_at: string,
+    updated_at: string,
+    status: string
+  },
+  setExpandedOrder?: Function,
 }
 
-export default function Order({orderDetails}: { orderDetails: OrderType}):JSX.Element {
-  return <div className='order-container'>
-      <p className="order customer-container">
+export default function Order({orderDetails, setExpandedOrder }:  OrderType): ReactNode {
+  const [expand, setExpand] = useState<boolean>(false)
+  return <label className={`order-container ${expand && 'expanded'}`}>
+      <input type="checkbox" name="" id="" onChange={(e) => !!setExpandedOrder && setExpandedOrder( (e.target.checked ? orderDetails : undefined) )} />
+      <div className="order customer-container">
         <h5>{orderDetails.customer}</h5>
         <small>{orderDetails.id}</small>
-      </p>
+      </div>
       <p className='order'>{orderDetails.updated_at}</p>
       <p className='order'>{orderDetails.branch}</p>
       <p className="order status">{orderDetails.status}</p>
       <p className="order price">231.51</p>
-  </div>
+      <p className='order arrow'> ⮜ </p>
+      { expand && <>
+        <p className="order">priority</p>
+        <p className="order">{orderDetails.order_type}</p>
+        <p className="order">{orderDetails.source}</p>
+        <p className="order">{orderDetails.date}</p>
+        <p className="order">{orderDetails.time}</p>
+        <p className="order">{orderDetails.notes}</p>
+      </>}
+  </label>
 }
