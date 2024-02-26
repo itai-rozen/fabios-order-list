7892057acbb44a02e4588ca725deec75123e15fa
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-27 01:18:31 +0200

    added another order cpt that displays checked order with more details

--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -1,15 +1,25 @@
-import './order-list.css'
-import { ReactNode } from 'react'
-import Order from '../Order/Order'
-import {data} from './../../data.json'
-export default function OrderList():ReactNode {
-  
+import './order-list.css';
+import { ReactNode, useState } from 'react';
+import Order, { OrderType } from '../Order/Order';
+import { data } from './../../data.json';
+
+
+export default function OrderList(): ReactNode {
+  const [exapandedOrder, setExpandedOrder] = useState<OrderType|undefined>(undefined)
   return (
-    <main className='order-list-container'>
+    <section className="order-list-container">
+      <aside className={exapandedOrder && 'expanded-order'}>
+      { 
+        exapandedOrder && <Order orderDetails={exapandedOrder as any} />
+      }
+      </aside>
+      <main className='order-list'>
       {
         data.results.map(order => {
-          return <Order orderDetails={order} />
+          return <Order key={order.id} setExpandedOrder={setExpandedOrder} orderDetails={order} />
         })
       }
-    </main>
+      </main>
+
+    </section>
   )}
