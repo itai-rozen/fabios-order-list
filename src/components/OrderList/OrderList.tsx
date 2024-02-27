826e0302958f9abcb06eb8dfdcc297958ed7c9e2
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-27 02:28:24 +0200

    added query client

--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -2,13 +2,15 @@ import './order-list.css';
 import { ReactNode, useState } from 'react';
 import Order, { OrderType } from '../Order/Order';
 import { data } from './../../data.json';
+import { useQueryClient } from '@tanstack/react-query';
 
 
 export default function OrderList(): ReactNode {
   const [exapandedOrder, setExpandedOrder] = useState<OrderType|undefined>(undefined)
+  const queryClient = useQueryClient();
   return (
     <section className="order-list-container">
-      <aside className={exapandedOrder && 'expanded-order'}>
+      <aside className={`aside ${exapandedOrder && 'expanded-order'}`}>
       { 
         exapandedOrder && <Order orderDetails={exapandedOrder as any} />
       }
