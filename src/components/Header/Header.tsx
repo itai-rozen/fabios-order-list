896b0a9b3d22eb3a8f481ac782868231e3090ea2
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-27 13:24:14 +0200

    added form as a modal that pops on state change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,18 @@
+import OrderForm from '../OrderForm/OrderForm'
 import './header.css'
+import { useState } from 'react'
 
 export default function Header() {
+  const [showForm, setShowForm] = useState<boolean>(false)
   return <div className='header-container'>
     <div className="btn-container">
-      <button>add order +</button>
+      <button onClick={() => setShowForm(true)}>add order +</button>
     </div>
     <div className="inputs-container">
       <input type="text" placeholder='free text' />
       <input type="datetime" name="" id="" placeholder='01/02/2024' />
       <select name="" aria-placeholder='filter' id=""></select>
     </div>
+    {showForm && <OrderForm setShowForm={setShowForm} />}
   </div>
 }
