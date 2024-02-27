7a8d4087602b5c32d01754df45f86f8903596135
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-27 13:25:59 +0200

    getting orders using react query

--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -1,15 +1,22 @@
 import './order-list.css';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useState, useEffect } from 'react';
 import Order, { OrderType } from '../Order/Order';
-import { data } from './../../data.json';
-import { useQueryClient } from '@tanstack/react-query';
+// import { data } from './../../data.json';
+import { useQuery } from '@tanstack/react-query';
+import { getOrders } from './../../api';
 
 
 export default function OrderList(): ReactNode {
+  const { data: orders, isLoading} = useQuery({
+    queryFn: () => getOrders(),
+    queryKey: ["orders"]
+  })
   const [exapandedOrder, setExpandedOrder] = useState<OrderType|undefined>(undefined)
-  const queryClient = useQueryClient();
+
+
   return (
     <section className="order-list-container">
+      {isLoading && <div>Loading...</div>}
       <aside className={`aside ${exapandedOrder && 'expanded-order'}`}>
       { 
         exapandedOrder && <Order orderDetails={exapandedOrder as any} />
@@ -17,7 +24,7 @@ export default function OrderList(): ReactNode {
       </aside>
       <main className='order-list'>
       {
-        data.results.map(order => {
+        orders?.map((order:OrderType)  => {
           return <Order key={order.id} setExpandedOrder={setExpandedOrder} orderDetails={order} />
         })
       }
