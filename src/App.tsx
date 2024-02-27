befeea6623077ef1d4dfffc75f35d65e8519ac05
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-27 02:27:55 +0200

    wrapped everything with quert provider, init query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { useState, useEffect } from 'react'
-import {data} from './data.json'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { data } from './data.json'
 import './App.css'
 import OrderList from './components/OrderList/OrderList'
 import Header from './components/Header/Header'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      retry: 1,
+    },
+  },
+});
 function App() {
-  useEffect(() => {
-    console.log('data:', data)
-  }, [])
+
   return (
-    <div className='app-container'>
-      <Header />
-      <OrderList />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className='app-container'>
+        <Header />
+        <OrderList />
+      </div>
+    </QueryClientProvider>
   )
 }
 
