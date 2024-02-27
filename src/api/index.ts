fa00d6e9148965e2c383da4130a01b4815321a4d
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-27 15:26:56 +0200

    added zod safeparse to edit & create funcs

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,32 @@
 import { formInputsInterface } from "../components/OrderForm/OrderForm";
-
+import { string, z } from "zod";
 const URL = 'https://61c2ed619cfb8f0017a3e77d.mockapi.io/contacts'
 export async function getOrders() {
   const res =   await (await fetch(URL)).json()
   return res
 }
 
+
 export async function createOrder(req: any) {
-  const method = 'POST';
-  const headers = {
-    'Content-Type': 'application/json'
-  };
-  const body = JSON.stringify(req);
+  const NewOrderSchema = z.object({
+    customer: z.string(),
+    branch: z.string(),
+    notes: z.string(),
+    source: z.string(),
+    order_type: z.string()
+  })
   try {
+    const validatedOrder = NewOrderSchema.safeParse(req)
+    console.log('validated order:' , validatedOrder)
+    if (!validatedOrder.success)
+      throw new Error(validatedOrder.error.message)
+    const method = 'POST';
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+
+    const body = JSON.stringify( validatedOrder.success && validatedOrder.data);
+
     const res = await fetch(URL, {body, method, headers});
     console.log('res: ', res)
   } catch(err) {
@@ -35,13 +49,26 @@ export async function deleteOrder(id: string) {
 }
 
 export async function editOrder(orderDetails: formInputsInterface) {
-  const orderId = orderDetails.id;
-  const body = JSON.stringify(orderDetails);
-  const method = 'PUT';
-  const headers = {
-    'Content-Type': 'application/json'
-  };
+  const EditOrderSchema = z.object({
+    id: z.string(),
+    customer: z.string(),
+    branch: z.string(),
+    notes: z.string(),
+    source: z.string(),
+    order_type: z.string()
+  })
   try {
+    const validatedOrder = EditOrderSchema.safeParse(orderDetails)
+    if (!validatedOrder.success)
+      throw new Error(validatedOrder.error.message);
+
+    const orderId = orderDetails.id;
+    const body = JSON.stringify(validatedOrder.data);
+    const method = 'PUT';
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+
     await fetch( `${URL}/${orderId}`, {body, method, headers});
   } catch(err) {
     console.log('err @editOrder(): ',err);
