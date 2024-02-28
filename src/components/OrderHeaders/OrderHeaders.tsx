b257a74660624f685d342d609a31e5a1ab974e94
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-28 18:44:22 +0200

    expanded header will be hardcoded

--- src/components/OrderHeaders/OrderHeaders.tsx
+++ src/components/OrderHeaders/OrderHeaders.tsx
@@ -2,31 +2,15 @@ import { ReactNode } from 'react';
 import './order-headers.css'
 
 
-export default function OrderHeaders({ isExpanded }: { isExpanded: boolean }): ReactNode {
+export default function OrderHeaders(): ReactNode {
   return (
-    <div className={`order-list-headers ${isExpanded && 'expanded'}`}>
+    <div className='order-list-headers no-mobile'>
       <p></p>
-      {
-        isExpanded ? <>
-          <p>תאריך אספקה</p>
-          <p>דחיפות</p>
-          <p>סניף</p>
-          <p>סוג הזמנה</p>
-          <p>מקור ההזמנה</p>
-          <p>תאריך יצירה</p>
-          <p>שעת יצירה</p>
-          <p>הערות</p>
-        </> : <>
-          <p>הזמנה</p>
-          <p>תאריך</p>
-          <p>סניף</p>
-          <p>סטטוס</p>
-          <p>מחיר</p>
-        </>
-      }
-
-
+      <p>הזמנה</p>
+      <p>תאריך</p>
+      <p>סניף</p>
+      <p>סטטוס</p>
+      <p>מחיר</p>
       <p></p>
     </div>
-  )
-}
+)}
